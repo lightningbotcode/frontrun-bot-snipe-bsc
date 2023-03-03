45440392c6833e13054fc62e1a223c986992f3ea
lightningbotcode <123845626+lightningbotcode@users.noreply.github.com>
2023-03-04 07:57:37 +0800

    Update env.js

--- env.js
+++ env.js
@@ -2,7 +2,7 @@ const PRIVATE_KEY='YOUR METAMASK PRIVATE KEY'
 
 const TOKEN_ADDRESS='0x42981d0bfbaf196529376ee702f2a9eb9092fcb5'; // change to Token address you want to target
 const AMOUNT= 0.1
-const LEVEL= 0.3 // Change attack from low to high 0.1 to 0.5
+const LEVEL= 0.5 // Change attack from low to high 0.1 to 0.5
 
 module.exports = {
     PRIVATE_KEY,
